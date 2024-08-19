bf1e13a158b52c1557cffc998dd31517820cb903
natanael <rojonatanael99@gmail.com>
2024-08-18 22:35:31 -0400

    feat(formating dates): Implementing an end point to format dates

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+require('dotenv').config();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+let cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +19,22 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// app.get("/api/hello", function (req, res) {
+//   res.json({ greeting: 'hello API' });
+// });
+
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string || Date.now();
+  let date = new Date(dateString);
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+// Listen on port set in environment letiable or default to 3000
+let listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
